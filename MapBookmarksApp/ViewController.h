6b1be6638f5dacb33621da9c43f1e95d18f12158
Pavel Ostanin <pavel.os@kttsoft.com>
2015-11-09 08:45:38 +0200

    Initial Commit

---
+++ MapBookmarksApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MapBookmarksApp
+//
+//  Created by Pavel Ostanin on 09/11/2015.
+//  Copyright © 2015 PavelOstanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ MapBookmarksApp/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MapBookmarksApp
+//
+//  Created by Pavel Ostanin on 09/11/2015.
+//  Copyright © 2015 PavelOstanin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
